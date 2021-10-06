876c27f7a7af23d3aff799a249d160002f4d5d8e
Andrew Russell <67603030+ARussell-CF@users.noreply.github.com>
2021-10-06 11:53:04 -0400

    Rough outline

--- js/site.js
+++ js/site.js
@@ -1,17 +1,37 @@
 //get starting values from the screen
 //Controller Function
-function getValues() {
-    Alert("Hello App");
-}
+function getUserString() {
+    //Get the user's input
+    let userString = "" //You write this
 
-function getValues2() {
-    let msg = "";
+    //Reverse the user's input - separate function
+    let revString = reverseString(userString);
 
-    msg = document.getElementById("message").value;
+    //Compare original input and reversed input - separate function
+    let result = compareStrings(userString, revString);
 
-    Swal.fire(
-        'APP NAME',
-        msg,
-        'success'
-    )
-}
+    //Output Result - separate function
+    displayData(result);
+
+};
+
+function reverseString(userString){
+    //This is Rewind    
+};
+
+function compareStrings(userString, revString){
+    //This is the work part
+    //A != a
+    //Normalize the case
+    //Extra credit - "A man, a plan, a canal; Panama" - is a palindrome when the spaces and special character
+    //  are removed
+    //RegEx is used for this - Google is your friend
+    
+};
+
+function displayData(result){
+    //We want to output three things
+    //1. The original input
+    //2. The reversed input
+    //3. Was it a palindrome
+};
